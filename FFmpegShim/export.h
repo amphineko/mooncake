7b508091cd5850eda02c3656c5acadf05dce13e0
amphineko <sukareki@gmail.com>
2019-12-18 11:38:49 +0800

    support gcc compilation

--- FFmpegShim/export.h
+++ FFmpegShim/export.h
@@ -1,6 +1,10 @@
 #ifndef FFMPEGSHIM_EXPORT_H
 #define FFMPEGSHIM_EXPORT_H
 
-#define LIBRARY_API(ret_type) extern "C" __declspec(dllexport) ret_type __stdcall
+#if defined(_MSC_VER)
+#define LIBRARY_API(ret) __declspec(dllimport) ret __stdcall
+#elif defined(__GNUC__)
+#define LIBRARY_API(ret) __attribute__((visibility("default"))) ret
+#endif
 
 #endif
