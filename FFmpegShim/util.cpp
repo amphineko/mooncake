3b96545514093c201f590edda0dfd0dc5ff71287
amphineko <sukareki@gmail.com>
2019-12-18 11:39:33 +0800

    shim/util: refactor frame api

--- FFmpegShim/util.cpp
+++ FFmpegShim/util.cpp
@@ -3,12 +3,26 @@
 extern "C"
 {
 #include <libavutil/frame.h>
+#include <libavutil/imgutils.h>
 #include <libavutil/mem.h>
 }
 
-LIBRARY_API(AVFrame *) shim_alloc_frame()
+LIBRARY_API(AVFrame *) shim_frame_alloc()
 {
     return av_frame_alloc();
+}
+
+LIBRARY_API(int) shim_frame_alloc_buffer(int w, int h, int pix_fmt, AVFrame *frame)
+{
+    frame->format = pix_fmt;
+    frame->width = w;
+    frame->height = h;
+    return AVERROR(av_frame_get_buffer(frame, 32));
+}
+
+LIBRARY_API(void) shim_frame_free(AVFrame *frame)
+{
+    av_frame_free(&frame);
 }
 
 LIBRARY_API(void) shim_free(void *ptr)
--- Mooncake/FFmpegShim/AVFrame.cs
+++ Mooncake/FFmpegShim/AVFrame.cs
@@ -7,7 +7,12 @@ namespace AtomicAkarin.Mooncake.FFmpegShim
     {
         public AVFrame()
         {
-            Pointer = ShimAllocateFrame();
+            Pointer = ShimFrameAllocate();
+        }
+
+        public AVFrame(int width, int height, AVPixelFormat pixelFormat) : this()
+        {
+            ShimFrameAllocateBuffer(width, height, (int) pixelFormat, Pointer);
         }
 
         public IntPtr Pointer { get; }
@@ -18,12 +23,21 @@ namespace AtomicAkarin.Mooncake.FFmpegShim
             GC.SuppressFinalize(this);
         }
 
-        [DllImport(ShimUtil.LibraryName, EntryPoint = "shim_alloc_frame")]
-        private static extern IntPtr ShimAllocateFrame();
+        // LIBRARY_API(AVFrame *) shim_frame_alloc()
+        [DllImport(ShimUtil.LibraryName, EntryPoint = "shim_frame_alloc")]
+        private static extern IntPtr ShimFrameAllocate();
+
+        // LIBRARY_API(int) shim_frame_alloc_buffer(int w, int h, int pix_fmt, AVFrame *frame)
+        [DllImport(ShimUtil.LibraryName, EntryPoint = "shim_frame_alloc_buffer")]
+        private static extern void ShimFrameAllocateBuffer(int w, int h, int pixFmt, IntPtr frame);
+
+        // LIBRARY_API(void) shim_frame_free(AVFrame *frame)
+        [DllImport(ShimUtil.LibraryName, EntryPoint = "shim_frame_free")]
+        private static extern void ShimFrameFree(IntPtr frame);
 
         private void Free()
         {
-            ShimUtil.FreeObject(Pointer);
+            ShimFrameFree(Pointer);
         }
 
         ~AVFrame()
