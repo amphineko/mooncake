ae8c99b7bc2b70beb2f2c80c61c0a81988f2d530
amphineko <sukareki@gmail.com>
2019-12-19 14:07:01 +0800

    fix incorrect export

--- FFmpegShim/export.h
+++ FFmpegShim/export.h
@@ -2,9 +2,9 @@
 #define FFMPEGSHIM_EXPORT_H
 
 #if defined(_MSC_VER)
-#define LIBRARY_API(ret) __declspec(dllexport) ret __stdcall
+#define LIBRARY_API(ret) extern "C" __declspec(dllexport) ret __stdcall
 #elif defined(__GNUC__)
-#define LIBRARY_API(ret) __attribute__((visibility("default"))) ret
+#define LIBRARY_API(ret) extern "C" __attribute__((visibility("default"))) ret
 #endif
 
 #endif
